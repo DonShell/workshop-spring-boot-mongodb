4ce3dad2fe66be6bd5086f36bf5fecbd406f7591
DonShell <assum.dev@protonmail.com>
2023-02-09 09:28:50 -0300

    Usando padrão DTO para retornar usuários

---
+++ src/main/java/com/example/demo/dto/UserDTO.java
@@ -0,0 +1,42 @@
+package com.example.demo.dto;
+
+import java.io.Serializable;
+
+import com.example.demo.domain.User;
+
+public class UserDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	private String id;
+	private String email;
+	private String name;
+	
+	UserDTO(){}
+	
+	public UserDTO(User obj)
+	{
+		id = obj.getId();
+		name= obj.getName();
+		email = obj.getEmail();
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
